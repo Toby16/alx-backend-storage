2d779e06e59cd72f8a4fe9338a237b23f57f9542
Toby16 <bolajibaasit14@gmail.com>
2023-08-10 19:07:03 -0400

    We are all unique! | Report: Task Done

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,7 @@
--- Check if the table 'users' already exists
-IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
-    -- Create the 'users' table
-    CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        name VARCHAR(255)
-    );
-END IF;
+-- Creates a table with unique users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
