d72eda0caab29b1d2abf300e332fd46a77c1d2b3
Toby16 <bolajibaasit14@gmail.com>
2023-08-16 20:38:56 -0400

    Retrieving lists | Report: Task Done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,7 @@ Writing strings to Redis
 Reading from Redis and recovering original type
 Incrementing values
 Storing lists
+Retrieving lists
 """
 
 import redis
@@ -101,3 +102,26 @@ if __name__ == "__main__":
 
     local_redis = redis.Redis()
     print(local_redis.get(key))
+
+
+def replay(fn: Callable) -> None:
+    """
+    Displays the call history of a Cache class method.
+    """
+    if fn is None or not hasattr(fn, "__self__"):
+        return
+    redis_store = getattr(fn.__self__, "_redis", None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    fxn_name = fn.__qualname__
+    in_key = "{}:inputs".format(fxn_name)
+    out_key = "{}:outputs".format(fxn_name)
+    fxn_call_count = 0
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
+    print("{} was called {} times:".format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+        print("{}(*{}) -> {}".format(fxn_name, fxn_input.decode("utf-8"),
+                                     fxn_output,))
